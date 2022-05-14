ffec8972e92252195f5692045391c1e4a2adbce7
vianney yovo <vianneyyovo@gmail.com>
2022-05-14 19:01:37 +0000

    Update convert_c_percent_p.c

--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -12,16 +12,27 @@ unsigned int convert_p(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_c - Converts an argument to an unsigned char and
+
  *             stores it to a buffer contained in a struct.
+
  * @args: A va_list pointing to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_c(va_list args, buffer_t *output,
@@ -61,16 +72,27 @@ unsigned int convert_c(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_percent - Stores a percent sign to a
+
  *                   buffer contained in a struct.
+
  * @args: A va_list pointing to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer (always 1).
+
  */
 
 unsigned int convert_percent(va_list args, buffer_t *output,
@@ -108,16 +130,27 @@ unsigned int convert_percent(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_p - Converts the address of an argument to hex and
+
  *             stores it to a buffer contained in a struct.
+
  * @args: A va_list pointing to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_p(va_list args, buffer_t *output,
