1392db1aebd9c86bd418c446ccf7c360080b70d3
vianney yovo <vianneyyovo@gmail.com>
2022-05-14 18:57:31 +0000

    Update convert_numbers.c

--- convert_numbers.c
+++ convert_numbers.c
@@ -21,16 +21,27 @@ unsigned int convert_o(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_di - Converts an argument to a signed int and
+
  *              stores it to a buffer contained in a struct.
+
  * @args: A va_list pointing to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_di(va_list args, buffer_t *output,
@@ -154,16 +165,27 @@ unsigned int convert_di(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_b - Converts an unsigned int argument to binary
+
  *             and stores it to a buffer contained in a struct.
+
  * @args: A va_list pointing to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_b(va_list args, buffer_t *output,
@@ -191,16 +213,27 @@ unsigned int convert_b(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_o - Converts an unsigned int to octal and
+
  *             stores it to a buffer contained in a struct.
+
  * @args: A va_list poinitng to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_o(va_list args, buffer_t *output,
@@ -256,16 +289,27 @@ unsigned int convert_o(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_u - Converts an unsigned int argument to decimal and
+
  *               stores it to a buffer contained in a struct.
+
  * @args: A va_list pointing to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_u(va_list args, buffer_t *output,
