d98b382d06b9674f4d15686d7af61351c28cb9d8
vianney yovo <vianneyyovo@gmail.com>
2022-05-14 18:55:29 +0000

    Update convert_strings.c

--- convert_strings.c
+++ convert_strings.c
@@ -18,16 +18,27 @@ unsigned int convert_R(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_s - Converts an argument to a string and
+
  *             stores it to a buffer contained in a struct.
+
  * @args: A va_list pointing to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_s(va_list args, buffer_t *output,
@@ -95,19 +106,33 @@ unsigned int convert_s(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_S - Converts an argument to a string and
+
  *             stores it to a buffer contained in a struct.
+
  * @args: A va_list pointing to the argument to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  *
+
  * Description: Non-printable characteres (ASCII values < 32 or >= 127)
+
  *              are stored as \x followed by the ASCII code value in hex.
+
  */
 
 unsigned int convert_S(va_list args, buffer_t *output,
@@ -185,16 +210,27 @@ unsigned int convert_S(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_r - Reverses a string and stores it
+
  *             to a buffer contained in a struct.
+
  * @args: A va_list pointing to the string to be reversed.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A length modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_r(va_list args, buffer_t *output,
@@ -262,16 +298,27 @@ unsigned int convert_r(va_list args, buffer_t *output,
 
 
 /**
+
  * convert_R - Converts a string to ROT13 and stores
+
  *             it to a buffer contained in a struct.
+
  * @args: A va_list pointing to the string to be converted.
+
  * @flags: Flag modifiers.
+
  * @wid: A width modifier.
+
  * @prec: A precision modifier.
+
  * @len: A lenth modifier.
+
  * @output: A buffer_t struct containing a character array.
+
  *
+
  * Return: The number of bytes stored to the buffer.
+
  */
 
 unsigned int convert_R(va_list args, buffer_t *output,
